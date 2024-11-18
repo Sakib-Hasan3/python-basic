7590afb05505cbde83ba3641da89b82cd63493be
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-19 03:35:23 +0600

    Add files via upload

---
+++ booleantype.py
@@ -0,0 +1,28 @@
+#boolean data type
+
+Bool= True   #true is defined with Alphabetic letter T
+print(Bool)
+
+print(type(Bool))
+
+#another examle 
+
+bool= False   #true is defined with Alphabetic letter T
+print(bool)
+
+print(type(bool))
+
+#another example 
+
+x=8
+y=10
+
+print(x>y)
+
+print(type(x>y))
+
+#another example 
+
+x=10
+y=10
+print(x==y)
