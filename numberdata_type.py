2d1e8fc2bff8d0ca839207f3de080161fa95c1a6
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-19 03:05:44 +0600

    Add files via upload

---
+++ numberdata_type.py
@@ -0,0 +1,21 @@
+#int data type
+
+x=420
+print(x)
+
+print(type(x)) #which type of data define
+
+#float data type 
+
+y=20.4
+print(y)
+
+print(type(y)) #which type of data define
+
+#complex type data
+#with j all other data types are complex
+
+z=420j
+print(z)
+
+print(type(z))
