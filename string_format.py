22ce07315ae289218d417ff4e4245367cb9e7f0f
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-19 03:40:38 +0600

    Add files via upload

---
+++ string_format.py
@@ -0,0 +1,11 @@
+#string format 
+
+num1=20
+num2=50
+print("This is my super number " , num1+num2)
+
+#another example
+
+num1=50
+num2=50
+print(f"Thisi is my super number {num1+num2}") #using f before the string and using mathmatical operation in the second bracket of the string  
