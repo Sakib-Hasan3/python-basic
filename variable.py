268acd1551011862f8e52902a282541a0372bdc5
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-19 01:10:44 +0600

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,25 @@
+# step 1
+
+name="Hey progarammer"
+print(name);
+
+#step 2
+
+name=123456;
+print(name);
+
+#step 3
+
+name=123456;
+print("name");
+
+#step 4
+
+name=123456;
+hablu="hey hablu prorammer"
+print(name);
+
+#step 5
+
+name=123456;
+print("name");
