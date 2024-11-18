32a65016b8fcefadfa4409e1c9e6dfef10eb1361
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-19 03:26:50 +0600

    Add files via upload

---
+++ stringtype.py
@@ -0,0 +1,11 @@
+#string data type 
+
+yourname="Sakib"
+print(yourname)
+
+myname="Hasan"
+print(myname)
+
+print(yourname+ myname)  #add two string together 
+
+print('My name is ' + ' ' +myname) #add string with space in between 
