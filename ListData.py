4ebf4ff09cb4e428766e921f3cab68cd2e9adda9
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-20 02:50:50 +0600

    Add files via upload

---
+++ ListData.py
@@ -0,0 +1,26 @@
+#list Data type
+
+li=[1,2,3]
+print(type(li))
+print(li)
+
+#another way 
+
+li=["A","B","C"]
+print(type(li))
+print(li)
+
+
+#change index variable 
+
+li=[1,2,3]
+li[1]=10
+print(type(li))
+print(li)
+
+
+#boolean string 
+
+list=[True,False,True,True]
+print(type(list))
+print(list)
