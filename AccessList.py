a6b34e2d8ad72036d83fde5f977aa8803d728c58
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-20 02:56:25 +0600

    Add files via upload

---
+++ AccessList.py
@@ -0,0 +1,16 @@
+#access list of tuples
+#direct access data from the list
+
+d=["x","y","z","w"]
+print(d[1])
+
+#another way 
+
+d=["x","y","z","w"]
+print(type(d))
+print(d[3])
+
+#change list item
+
+d[0]="a"
+print("Now the list is : " ,d)
