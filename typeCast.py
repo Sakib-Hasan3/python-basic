4515955787ca66484ef73208b8c84a21aa25c795
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-20 02:36:56 +0600

    Add files via upload

---
+++ typeCast.py
@@ -0,0 +1,19 @@
+#type cast= convert
+#int() = for int value
+#float() = for float value
+
+
+name="12345"  #using type cast it should be declare number in " " mark because it can't convert string into number
+print(name)
+
+print(type(name))
+
+#another way 
+
+num="50"
+print(float(num))
+
+#another way 
+
+x="5.5"
+print(int(x))  #output error
