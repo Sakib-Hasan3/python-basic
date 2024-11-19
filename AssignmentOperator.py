7863968eb87f7ee0569a7383147e3de8c61ed549
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-20 02:12:46 +0600

    Add files via upload

---
+++ AssignmentOperator.py
@@ -0,0 +1,26 @@
+#assignment opearator
+
+d=5
+sum=d+5
+print(sum)
+
+a=10
+sum +=10
+print(sum)
+
+
+#now substraction
+a=10
+sum -=10
+print(sum) 
+
+#comparison operator
+
+x=5
+y=3
+print(x==y)
+
+print(x!=y)
+
+
+
