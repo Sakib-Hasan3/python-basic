8c890be11ddd15a91b6c19192a9bb189dd256793
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-19 23:15:11 +0600

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,26 @@
+#python operator
+
+a=20
+b=10
+
+#Addition operator
+
+print(f"the total number is = ",a+b )
+
+#susbtraction operator 
+
+print(f"the substraction number is  =" ,a-b)
+
+
+#multiplication operator
+
+print(f" the multiplication is = ", a*b)
+
+
+#division 
+
+print(f" the division is = ", a/b)
+
+#remainder
+
+print(f" the remainder is = ", a%b)
