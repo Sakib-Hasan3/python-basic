d6e20de1330e79a0769855cd60cbf63b6f2018d9
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-20 02:22:31 +0600

    Add files via upload

---
+++ UserInput.py
@@ -0,0 +1,13 @@
+#input from user
+
+username=input()
+print(username)
+
+
+#another way
+
+
+username=input("Enter your username : ")
+password=input("Enter your password :")
+print(username)
+print(password)
