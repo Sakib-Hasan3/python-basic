a665fe8041bd3ec125dfe0741805a5db4d9dae52
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-21 03:10:46 +0600

    Add files via upload

---
+++ TupleData.py
@@ -0,0 +1,18 @@
+#tuple data type in python
+
+NewTuples=(1,2,3,"eshan",False) #list data is not changeable in tuple
+
+print(type(NewTuples))
+
+
+#no variable can be changed
+
+NewTuples[1]=20
+print(NewTuples)
+
+#negative index
+#-1 means the last number of list
+
+xTuples=(1,2,"Sakib","eshan","False")
+print(xTuples[-1])
+
