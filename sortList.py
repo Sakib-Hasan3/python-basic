e9f1c5cdee4d13ba257f24fea11433f080b8875a
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-20 19:26:12 +0600

    Add files via upload

---
+++ sortList.py
@@ -0,0 +1,16 @@
+#sort list in python
+#x.sort() to sort list in ascending order
+
+x=[10,1,2,5,7,8,4]
+x.sort()
+print(x)
+
+y=["b","c","f","a","k"]
+y.sort()
+print(y)
+
+#reverse list
+
+num=[1,2,3,4,5,6,7]
+num.reverse()
+print(num)
