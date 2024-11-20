4cf957c7464f1ba95b708dab2745811c6e3b5455
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-20 19:44:13 +0600

    Add files via upload

---
+++ listExercise.py
@@ -0,0 +1,17 @@
+#print the second elements in the list
+
+fruits =["apple","bannana","cherry"]
+print(fruits[1])
+
+
+#change value apple  to orange in the list 
+
+fruits =["apple","bannana","cherry"]
+fruits[0]="orange" 
+print(fruits)
+
+#add the append method and kiwi int the list
+
+fruits =["apple","bannana","cherry"]
+fruits.append("kiwi")
+print(fruits)
