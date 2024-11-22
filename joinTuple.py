ae120bd79a51eeb9166f38a9a9e5d08777db4def
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-22 12:27:54 +0600

    Add files via upload

---
+++ joinTuple.py
@@ -0,0 +1,16 @@
+x=(1,2,3)
+y=("a","b","c")
+
+z=x+y
+print(z)
+print(z*3)
+
+#another  way
+
+a = [10, 9, 8, 7]
+b = [4, 5, 6, 7]
+
+a.extend(b)  # Extend the list `a` with elements of `b`
+
+print(a)
+
