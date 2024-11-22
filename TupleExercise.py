6d721c8eac117e3200c06e44be0e9e2840132f20
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-22 12:50:01 +0600

    Add files via upload

---
+++ TupleExercise.py
@@ -0,0 +1,10 @@
+x = ("a", "b", "c")
+print(x[0])          # Accessing the first element of the tuple
+
+print(len(x))        # Getting the length of the tuple
+
+y = x.count("a")     # Counting occurrences of "a" in the tuple
+print(y)
+
+z = x.index(2)       # Trying to find the index of 2 in the tuple
+print(z)
