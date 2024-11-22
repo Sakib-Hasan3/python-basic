81d1de2a4a49fc78f82322f5478bf4214967a736
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-22 20:00:33 +0600

    Add files via upload

---
+++ accessSet.py
@@ -0,0 +1,17 @@
+#access set item in python
+
+x={"abul","babul","damn"}
+
+for i in x:
+    print(i)
+
+#another way 
+
+x={1,2,3,4,5,6}
+for i in x:
+    print(i)
+#another way 
+
+print(3 in x)
+    
+    
