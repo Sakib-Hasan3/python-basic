5d2bdc30fc9c65f7e1cc2c5202bd9132462c5f79
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-22 12:59:04 +0600

    Add files via upload

---
+++ set.py
@@ -0,0 +1,14 @@
+#set in python
+
+x= {1,True,"a","c"} 
+print(x)#set items are changeable but you can remove or add item
+
+#another way 
+
+x= {1,False,"a","a"} 
+
+print(x)
+
+print(type(x))
+
+print(len(x))
