3e1d3dfbc955d85451874443eb69cedb307be272
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-23 20:44:52 +0600

    Add files via upload

---
+++ RemoveSet.py
@@ -0,0 +1,17 @@
+#remove set item in python
+
+x={"a","b","c"}
+x.remove("b")
+print(x)
+x.pop()  # this will remove the last item in the list
+print(x)
+
+
+#another way in python
+
+y={1,2,3,4,5,6}
+y.remove(2)
+print(y)
+
+y.clear()
+print(y) #this will remove all items in the list 
