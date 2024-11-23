1bd16bd3400f67010bd8455ad15631d84edda1c7
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-24 04:04:55 +0600

    Add files via upload

---
+++ joinSet.py
@@ -0,0 +1,14 @@
+#added two set in one line
+
+x = {1,2,3,4}
+y={7,8,9,10}
+
+z=x.union(y) #union is used to add two set in one line
+print(z)
+
+
+#another way in the list
+
+y.update(x) #update is also added tw add the list
+
+print(y)
