2e9a2628c63c1a4242bfcb9282c69afe451e18c7
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-25 02:57:18 +0600

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,28 @@
+#loops in python
+
+#while loop in python
+
+x=0
+while x<5:
+    print("yes x is less than 10")
+    x=x+1
+    
+#for loop in python
+
+x = 5
+for i in range(0, 5):
+    print("This is Bangladesh")
+    
+#another for loop 
+
+y={1,2,3,4,5,6}
+for i  in y:
+    print(i)
+   
+   
+   #another for loop with string
+    
+fruits =["apple","banana","cherry"]
+for x in fruits:
+    print(x)
+    
