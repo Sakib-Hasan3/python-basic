1d029ad69b17d05393c7ad4cdda5b867e7a6cd96
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-25 03:21:20 +0600

    Add files via upload

---
+++ break.py
@@ -0,0 +1,16 @@
+#break statement in python
+
+for x in range(9):
+    
+    if x==3 :
+        break
+    print(x)
+    
+    #another way of break statement
+    
+Hablu=[1,2,3,4,5,6,7,8]
+
+for x in range(len(Hablu)):
+    if x==4:
+        break
+    print(x)
