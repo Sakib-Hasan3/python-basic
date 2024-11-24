3b038da8352a1a4d13bdc4fc9aebc716b7e08601
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-25 03:23:49 +0600

    Add files via upload

---
+++ continue.py
@@ -0,0 +1,16 @@
+#continue statement in python
+
+for x in range(9):
+    
+    if x==3 :
+        continue
+    print(x)
+    
+    #another way of continue statement
+    
+Hablu=[1,2,3,4,5,6,7,8]
+
+for x in range(len(Hablu)):
+    if x==4:
+        continue
+    print(x)
