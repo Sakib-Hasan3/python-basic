0345688f08141ff1920e4124f87a1ab16b692a11
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-24 23:23:48 +0600

    Add files via upload

---
+++ ifElseCondition.py
@@ -0,0 +1,37 @@
+#if else condition in python
+
+#one condition 
+
+a=30
+b=10
+
+if b>a :
+    print("b is greater than a")
+    
+else : 
+    print("a is greater than b")
+    
+#another condition 
+
+a=20
+b=10
+
+if b==a:
+    print("b is equal to a")
+    
+else : 
+    print("a is not equal to b")
+    
+    
+#another way 
+  
+a=30
+b=30
+
+if b!=a :
+    print("b is not equal to a")
+    
+else : 
+    print("a is equal to b")
+    
+    
