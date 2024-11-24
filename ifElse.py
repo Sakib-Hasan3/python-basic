f6480bd19d77013bcd95d23fdfc9cad8a6d18949
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-24 23:15:54 +0600

    Add files via upload

---
+++ ifElse.py
@@ -0,0 +1,21 @@
+#if else condition for loop
+
+x ={
+    "name": "John",
+    "age": 30,
+    "city" : "london"
+}
+
+x.get("city")
+print(x)
+
+#another way to do it
+
+print(x.get("city"))
+
+
+#update disctionaries
+
+x["city"] = "new york"
+print(x)
+
