4c0a84a1182aa2a57670b67be74cd3d32af0ffa8
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-24 19:21:43 +0600

    Add files via upload

---
+++ DictionariesCopy.py
@@ -0,0 +1,18 @@
+x = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+
+y = {
+    "name": "sakib",
+    "age": 22,
+    "city": "Bangladesh"
+    
+}
+
+y=x.copy()
+print(y)
+
+z= dict(x)
+print(z)
