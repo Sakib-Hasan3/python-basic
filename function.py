7ef6fdde06e6ec7652acb749fc5eef9f6f5fb0a9
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-25 03:16:33 +0600

    Add files via upload

---
+++ function.py
@@ -0,0 +1,32 @@
+# Define the function
+def x(a, b):
+    sum = a + b
+    print(sum)
+
+# Call the function multiple times
+x(20, 50)
+x(30, 50)
+x(40, 50)
+x(50, 50)
+x(60, 50)
+x(70, 50)
+x(80, 50)
+x(90, 50)
+x(100, 50)
+
+
+def minus(x,y):
+    z= x-y
+    print(z)
+
+# Call the function multiple times
+minus(20, 50)
+minus(30, 50)
+minus(40, 50)
+minus(50, 50)
+minus(60, 50)
+minus(70, 50)
+minus(80, 50)
+minus(90, 50)
+minus(100, 50)
+
