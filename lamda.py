df8c4b4a2ad8a5ddd7d25d593711c4640c139651
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-27 19:14:00 +0600

    Add files via upload

---
+++ lamda.py
@@ -0,0 +1,15 @@
+#defining the anonymous function
+
+
+#normal function type
+def x(a,b) :
+    sum=a+b
+    print(sum)
+    
+x(10,20)
+x(20,40)
+
+#lamda function
+
+y=lambda a,b,c,d: a+b+c+d
+print(y(50,20,10,15)) 
