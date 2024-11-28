aed29d3e98acd48cf61b2e3bd61acc01d3588018
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-28 23:53:28 +0600

    Add files via upload

---
+++ inheritece.py
@@ -0,0 +1,15 @@
+class Baba:
+    car = "Bmw"
+    tk = "100 crore"
+    home = "10 floor"
+    
+class Kaka(Baba):
+    BrokenPhone = " "
+    BrokenHome = " "
+    
+# Create an instance of Kaka and print the inherited car attribute
+k = Kaka()
+print(k.car)
+
+# Create an instance of Baba
+b = Baba()
