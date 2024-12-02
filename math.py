55a99ccc4438c6d46ff403114f7b43d021be9a46
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-12-02 20:57:16 +0600

    Add files via upload

---
+++ math.py
@@ -0,0 +1,14 @@
+a = min(30, 10, 20, 40, 100) #min() is used  to define the minimum number
+print(a)
+
+
+b = max(30, 10, 20, 40, 100) #max() is used to define the maximum number
+print(b)
+
+
+c=abs(-110) #abs is used to define the absolute value
+print(c)
+
+
+d=pow(5,3) #pow() is used to define the power of a number 
+print(d)
