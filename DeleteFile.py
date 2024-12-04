ac13e1b708ff18b8dce302aa94859354821f5d55
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-12-04 23:52:07 +0600

    Add files via upload

---
+++ DeleteFile.py
@@ -0,0 +1,12 @@
+#delete file in python
+
+import os
+os.remove("hablu.html") #the html file is deleted
+
+
+os.remove("dates.py") #the python file is deleted
+
+#rmdir is used to delete a directory
+ 
+import os 
+os.rmdir("hablu")  #this is used to delete the folder
