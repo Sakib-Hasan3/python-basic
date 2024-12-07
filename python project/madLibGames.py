a39711b079d74251ff7e87b2889f517f263060fb
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-12-07 19:00:20 +0600

    Add files via upload

---
+++ python project/madLibGames.py
@@ -0,0 +1,9 @@
+username = input("What is your name: ")
+fatherName = input("What is your father's name: ")
+motherName = input("What is your mother's name: ")
+study = input("Which class do you read in: ")
+relationship = input("What is your relationship status: ")
+
+print(f"My dog's name is {username}, my dog's father's name is {fatherName}, "
+      f"my dog's mother's name is {motherName}, my dog studies in class {study}, "
+      f"and our relationship is {relationship}.")
